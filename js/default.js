7431fc5680efa1cf1439689bd7d7b5b2947ecca3
cheroo <chinonnnonnon@gmail.com>
2017-06-19 17:18:54 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,2 +1,15 @@
-window.alert('こんにちは世界!');
-console.log('こんにちは世界!');
+var num=Math.floor(Math.random()*3);
+var ans=parseInt(windows.prompt('数あてゲーム。0~2の数字を入力してください!'));
+
+var mes;
+if(ans==num){
+  mes='あたり';
+}else if(ans<num){
+    mes='残念、もっと大きい';
+  }else if(num<ans){
+    mes='残念、もっと小さい';
+  }else{
+    mes='0~3の数字を入力してください';
+  }
+
+document.getElementById('choice').textContent=mes;
